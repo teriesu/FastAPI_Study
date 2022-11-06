e188a823580912d0232eeacf50a7f4955cac3800
Santiago Téllez <santiago99tellez@gmail.com>
2022-11-06 18:43:32 -0500

    Cookies an headers

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ from pydantic import EmailStr, HttpUrl, PaymentCardNumber
 
 # FastAPI
 from fastapi import FastAPI
-from fastapi import Body, Query, Path, Form
+from fastapi import Body, Query, Path, Form, Header, Cookie
 from fastapi import status
 
 app = FastAPI()
@@ -170,3 +170,29 @@ def login(
     password: str = Form(...)
     ):
     return LoginOut(username = username)
+
+# Cookies and Headers Parameters
+@app.post(
+    path = "/contact",
+    status_code = status.HTTP_200_OK
+)
+def contact(
+    first_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    last_name: str = Form(
+        ...,
+        max_length=20,
+        min_length=1
+    ),
+    email: EmailStr = Form(...),
+    message: str = Form(
+        ...,
+        min_length=20,
+    ),
+    user_agent: Optional[str] = Header(default = None),
+    ads: Optional[str] = Cookie(default=None)
+    ):
+    return user_agent
