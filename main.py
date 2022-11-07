c394f6ae4b07ce68ba7e9f500bd541e3db14b743
Santiago Téllez <santiago99tellez@gmail.com>
2022-11-06 21:28:01 -0500

    Path operations strucure users

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from uuid import UUID
 from datetime import date
 from datetime import datetime
-from typing import Optional
+from typing import Optional, List
 
 # Pydantic
 from pydantic import BaseModel
@@ -10,6 +10,7 @@ from pydantic import Field
 
 #FastAPI
 from fastapi import FastAPI
+from fastapi import status
 
 
 app = FastAPI()
@@ -39,6 +40,7 @@ class User(UserBase):
         max_length=30
     )
     birth_date: Optional[date] = Field(default = None)
+
 ## Tweet
 class Tweet(BaseModel):
     tweet_id: UUID = Field(...)
@@ -55,3 +57,69 @@ class Tweet(BaseModel):
 @app.get(path = "/")
 def home():
     return {"Primitive Twitter": "Working"}
+
+## Users
+@app.post( # El cliente envía la información del registro
+    path = "/signup",
+    response_model = User,
+    status_code=status.HTTP_201_CREATED,
+    summary= "Register a new user",
+    tags = ["Users"]
+)
+def signup():
+    pass
+
+@app.post( # El cliente envía la información del registro
+    path = "/Login",
+    response_model = User,
+    status_code=status.HTTP_200_OK,
+    summary= "Login a user",
+    tags = ["Users"]
+)
+def Login():
+    pass
+
+@app.get( # El cliente envía la información del registro
+    path = "/users",
+    response_model = List[User],
+    status_code=status.HTTP_200_OK,
+    summary= "Show all users",
+    tags = ["Users"]
+)
+def users():
+    pass
+
+@app.get( # El cliente envía la información del registro
+    path = "/users/{user_id}",
+    response_model = User,
+    status_code=status.HTTP_200_OK,
+    summary= "Show a user",
+    tags = ["Users"]
+)
+def show_a_user():
+    pass
+
+@app.delete( # El cliente envía la información del registro
+    path = "/users/{user_id}/delete",
+    response_model = User,
+    status_code=status.HTTP_200_OK,
+    summary= "Delete a user",
+    tags = ["Users"]
+)
+def delete_a_user():
+    pass
+
+
+
+@app.put( # El cliente envía la información del registro
+    path = "/users/{user_id}/update",
+    response_model = User,
+    status_code=status.HTTP_200_OK,
+    summary= "Update a user",
+    tags = ["Users"]
+)
+def update_a_user():
+    pass
+
+
+## Tweets
